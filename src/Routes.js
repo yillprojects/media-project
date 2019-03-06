3092590d08d83780f4eb597a41a3e048b99c3759
Olya Yakymovych <msolya@outlook.com>
2019-03-06 12:21:40 +0200

    added 404

--- src/Routes.js
+++ src/Routes.js
@@ -7,11 +7,13 @@ import {
 import { Redirect } from 'react-router';
 
 import LandingPage from './scenes/Landing/LandingPage.js';
+import NotFound from './scenes/404/NotFound.js';
 
 const Routes = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={LandingPage} />
+      <Route component={NotFound} />
     </Switch>
   </Router>
 );
