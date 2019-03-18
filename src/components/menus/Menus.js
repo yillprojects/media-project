f10141763b31258b27d3a58a50aa285af45d29ce
Olya Yakymovych <msolya@outlook.com>
2019-03-18 14:17:12 +0200

    added mobie sidebar

---
+++ src/components/menus/Menus.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import NavbarMenu from "./components/Navbar/Navbar.js";
+import Sidebar from "./components/Sidebar/Sidebar.js";
+
+const Menus = () => {
+	return [<Sidebar />, <NavbarMenu />];
+};
+
+export default Menus;
