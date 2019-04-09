a3190d33df7db592379cf707f7c4187f75f63e01
junkkerrigan <726721gfd>
2019-04-09 13:31:23 +0300

    added backend

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Redirect } from 'react-router';
 
@@ -8,6 +9,14 @@ import NotFound from './scenes/404/NotFound.js';
 import ProfilePage from './scenes/ProfilePage/ProfilePage.js';
 
 const Routes = () => {
+  // axios.post("http://localhost:8000/api/users/", {
+  //   username: 'a',
+  //   email: 'a@ukr.net',
+  //   password: '726721gfd',
+  //   id: '3'
+  // }).then(res => console.log(res.data));
+  // axios.get("http://localhost:8000/api/users/").then(res => console.log(res.data));
+
   const DefaultLayout = ({ component: Component }) => (
     <Route
       render={props => (
---
+++ src/backend/api/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+
+from .serializers import UserSerializer
+
+
+class UserView(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
