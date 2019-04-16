dd2f32e610878291d722fc92e6137b6aff921ba0
Olya Yakymovych <msolya@outlook.com>
2019-04-16 22:06:14 +0300

    added lost reduced

---
+++ src/redux/reducers/direction.reducer.js
@@ -0,0 +1,22 @@
+import { REHYDRATE } from "redux-persist";
+
+import { pageConstants } from "./../constants";
+
+export const direction = (state = { page: "1", userTab: "1" }, action) => {
+	switch (action.type) {
+		case pageConstants.CURRENT_PAGE:
+			return { ...state, page: action.page };
+
+		case pageConstants.CURRENT_PROFILE_TAB:
+			return { ...state, userTab: action.tab };
+
+		case REHYDRATE:
+			return {
+				page: action.payload.direction.page,
+				userTab: action.payload.direction.userTab
+			};
+
+		default:
+			return state;
+	}
+};
