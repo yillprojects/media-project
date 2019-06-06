19ff338cc0f549981e3af60473715d71d6b91b30
junkkerrigan <ander4ik@ukr.net>
2019-06-06 21:59:59 +0300

    updated search bar

--- src/components/menus/components/Navbar/components/SearchBox/components/Suggestions.js
+++ src/components/menus/components/Navbar/components/SearchBox/components/Suggestions.js
@@ -4,20 +4,27 @@ import { Link } from "react-router-dom";
 import { Button } from "reactstrap";
 import { FaRegSmileWink } from "react-icons/fa";
 
+import defaultAvatar from '../../../../../../../backend/static/profiles/defaultProfileAvatar.jpg';
+
+const host = 'http://localhost:8000/';
+
 const Suggestions = props => {
 	const options = props.results.map((person, index) => index <= 7 ? (
 		<li key={person.id} className="user-search-item notification-item">
 			<img
-				src="https://via.placeholder.com/30"
+				src={person.avatar? `${host}media/${person.avatar}` : defaultAvatar}
 				alt="user-img"
 				style={{ height: 30, width: 30 }}
 				className="notification-item-img"
 			/>
 			<div className="notification-event">
 				<Link to="#" className="notification-friend">
-					{person.username}
+					{`${person.first_name} ${person.last_name}`}
 				</Link>
-				<p className="chat-message-item">New York, NY</p>
+				<p className="chat-message-item">
+					{`${person.location.city? `${person.location.city.name}, ` : ''}
+					  ${person.location.country? person.location.country.name : ''}`}
+				</p>
 			</div>
 			<div className="notification-icon">
 				<Button className="send-request transparent-btn">
